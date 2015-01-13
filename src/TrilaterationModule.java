2b8a9a51770cbc16fe47eaa957a44aeac03608e8
boonum93 <boonum93@live.com>
2015-01-12 23:50:59 -0500

    Fixed bug with matrix a's calculation.
    Also calculated matrix c. Still need to calculate b.

--- src/TrilaterationModule.java
+++ src/TrilaterationModule.java
@@ -19,19 +19,38 @@ public class TrilaterationModule {
 //        Jama.Matrix orthogonal = qrDecomposition.getQ();
         
         Jama.Matrix a = new Matrix(positions[0].length, 1);
-        // Calculate a     
-        for(int i = 0; i < distances.length; i++) {
+        Jama.Matrix b = new Matrix(positions.length, positions[0].length);
+        Jama.Matrix c = new Matrix(positions[0].length, 1);
+        // Calculate a and B and c
+        for(int i = 0; i < positions.length; i++) {
             Jama.Matrix position = new Matrix(positions[i], positions[i].length);
             Jama.Matrix transposePosition = position.transpose();
+            Jama.Matrix posTimesTranspose = position.times(transposePosition);
+            double distanceSquared = distances[i]*distances[i];
             
-            Jama.Matrix leftTerm = position.times(transposePosition).times(position);
-            Jama.Matrix rightTerm = position.times(distances[i]*distances[i]);
-            
-            a.plusEquals(leftTerm.minus(rightTerm));
+            // a
+            Jama.Matrix aLeftTerm = posTimesTranspose.times(position);
+            Jama.Matrix aRightTerm = position.times(distanceSquared); 
+            a.plusEquals(aLeftTerm.minus(aRightTerm));
+  
+            // B
+            Jama.Matrix bLeftTerm = posTimesTranspose.times(-2);
+            //Jama.Matrix bMiddleTerm = ;
+            //Jama.Matrix bRightTerm = ;
+                    
+            // c
+            c.plusEquals(position);    
         }
+        
+        a.times(1.0/positions.length); // a's calculation done
+        b.times(1.0/positions.length); // b's calculation done
+        c.times(1.0/positions.length); // c's calculation done
         
         System.out.println("a");
         a.print(3, 3);
+        
+        System.out.println("c");
+        c.print(3, 3);
         
         return null;
     }
