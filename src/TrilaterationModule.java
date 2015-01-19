45173080de69e75d07169e198b548385494cf540
boonum93 <boonum93@live.com>
2015-01-19 18:43:16 -0500

    Completed the calculateLocation method.
    Removed RectCoordinates.java so that calculateLocation returns a 2d
    double array and not a RectCoordinates object.

--- src/TrilaterationModule.java
+++ src/TrilaterationModule.java
@@ -4,15 +4,15 @@ import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.MatrixUtils;
 
 public class TrilaterationModule {
-
+    private RealMatrix previousPosition;
     /**
      * Class constructor.
      */
     public TrilaterationModule() {
-
+        
     }
 
-    public RectCoordinates calculateLocation(double[] distances, double[][] positions) {
+    public double[][] calculateLocation(double[] distances, double[][] positions) {
         int numDimensions = positions[0].length;
         int numSamples = positions.length;
          
@@ -111,12 +111,10 @@ public class TrilaterationModule {
             
             // First q solution
             qSol1.setEntry(2, 0, (-quadraticB + sqrtTerm)/twoA);
-            System.out.println("q3Sol1=" + qSol1.getEntry(2, 0));
             qSol1.setEntry(0, 0, threeDimensionalSolveQ1(u, v, qSol1.getEntry(2, 0)));
             qSol1.setEntry(1, 0, threeDimensionalSolveQ2(u, v, qSol1.getEntry(2, 0)));
             // Second q solution
             qSol2.setEntry(2, 0, (-quadraticB - sqrtTerm)/twoA);
-            System.out.println("q3Sol2=" + qSol2.getEntry(2, 0));
             qSol2.setEntry(0, 0, threeDimensionalSolveQ1(u, v, qSol2.getEntry(2, 0)));
             qSol2.setEntry(1, 0, threeDimensionalSolveQ2(u, v, qSol2.getEntry(2, 0)));
             
@@ -127,34 +125,30 @@ public class TrilaterationModule {
             printRealMatrix(pSol1);
             System.out.println("pSol2");
             printRealMatrix(pSol2);
+            
+            RealMatrix solution;
+            
+            // If this is the first calculated position, then go through
+            // the longer process of using the distance formula to determine
+            // which of the solutions is most accurate. Otherwise, if it
+            // is not the first calculated position, then just return the 
+            // solution that is closest to the previous position.
+            if(previousPosition == null) {
+                System.out.println("First calculation");
+                solution = calculateBestSolution(distances, positions, pSol1, pSol2);
+            }
+            else {
+                System.out.println("After first calculation");
+                solution = solutionClosestToPrevious(pSol1, pSol2);
+            }
+            
+            previousPosition = solution;
+            
+            return solution.getData();
         }
         else if(numDimensions == 2) {
-            
-        }
-                
-        System.out.println("a");
-        printRealMatrix(a);
-        System.out.println("b");
-        printRealMatrix(b);
-        System.out.println("c");
-        printRealMatrix(c);
-        System.out.println("f");
-        printRealMatrix(f);
-        System.out.println("fPrime");
-        printRealMatrix(fPrime);
-        System.out.println("h");
-        printRealMatrix(h);
-        System.out.println("hPrime");
-        printRealMatrix(hPrime);
-        System.out.println("q");
-        printRealMatrix(q);
-        System.out.println("u");
-        printRealMatrix(u);
-        System.out.println("qT*q = " + qTransposeTimesQ);
-        System.out.println("v");
-        printRealMatrix(v);
-        
-        
+            // TODO: Write calculations for two-dimensions
+        }        
         return null;
     }
     
@@ -187,6 +181,64 @@ public class TrilaterationModule {
         
         return (-v2/u22) - ((u23/u22)*q3);
     }
+    
+    private RealMatrix calculateBestSolution(double[] distances, double[][] positions, RealMatrix solutionA, RealMatrix solutionB) {
+        // Increments whenever the particular solutions position is more accurate then the
+        // other for a given inputed position.
+        int countA = 0;
+        int countB = 0;
+        // Compare solutionA's position to solutionB's position by calculating the
+        // distances between each solution's position and each of the inputed positions
+        // to see which one is closest to the inputed distance measurements
+        for(int i = 0; i < positions.length; i++) {
+            double solADistance = Math.sqrt(Math.pow(solutionA.getEntry(0, 0) - positions[i][0], 2) +
+                    Math.pow(solutionA.getEntry(1, 0) - positions[i][1], 2) +
+                    Math.pow(solutionA.getEntry(2, 0) - positions[i][2], 2));
+            double solBDistance = Math.sqrt(Math.pow(solutionB.getEntry(0, 0) - positions[i][0], 2) +
+                    Math.pow(solutionB.getEntry(1, 0) - positions[i][1], 2) +
+                    Math.pow(solutionB.getEntry(2, 0) - positions[i][2], 2));
+            
+            double solADiff = solADistance - distances[i];
+            double solBDiff = solBDistance - distances[i];
+            // To prevent negative differences
+            if(solADiff < 0) {
+                solADiff = -solADiff;
+            }
+            if(solBDiff < 0) {
+                solBDiff = -solBDiff;
+            }
+            
+            if(solADiff <= solBDiff) {
+                countA++;
+            }
+            else {
+                countB++;
+            }
+        }
+        
+        if(countA >= countB) {
+            return solutionA;
+        }
+        else {
+            return solutionB;
+        }
+    }
+    
+    private RealMatrix solutionClosestToPrevious(RealMatrix solutionA, RealMatrix solutionB) {
+        double solADistance = Math.sqrt(Math.pow(solutionA.getEntry(0, 0) - previousPosition.getEntry(0, 0), 2) +
+                Math.pow(solutionA.getEntry(1, 0) - previousPosition.getEntry(1, 0), 2) +
+                Math.pow(solutionA.getEntry(2, 0) - previousPosition.getEntry(2, 0), 2));
+        double solBDistance = Math.sqrt(Math.pow(solutionB.getEntry(0, 0) - previousPosition.getEntry(0, 0), 2) +
+                Math.pow(solutionB.getEntry(1, 0) - previousPosition.getEntry(1, 0), 2) +
+                Math.pow(solutionB.getEntry(2, 0) - previousPosition.getEntry(2, 0), 2));
+        
+        if(solADistance <= solBDistance) {
+            return solutionA;
+        }
+        else {
+            return solutionB;
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -197,9 +249,21 @@ public class TrilaterationModule {
                                 {3., 0., 4.},
                                 {2., 5., 1.}};
         
-        double[] distances = {4.1231056256177, 3, 2.8284271247462, 3.3166247903554};
+        double[] distances1 = {4.1231056256177, 3, 2.8284271247462, 3.3166247903554};
         double[] distances2 = {3., 3., 4.5, 4};
         double[] distances3 = {4.2426, 3.7417, 5, 2.4495};
-        triMod.calculateLocation(distances2, positions);
+        double[] distances4 = {38.858, 36.689, 36.125, 33.615};
+        double[][] calcedLocation = triMod.calculateLocation(distances4, positions);
+        for(int i = 0; i < calcedLocation.length; i++) {
+            System.out.println(calcedLocation[i][0]);
+        }
+        calcedLocation = triMod.calculateLocation(distances1, positions);
+        for(int i = 0; i < calcedLocation.length; i++) {
+            System.out.println(calcedLocation[i][0]);
+        }
+        calcedLocation = triMod.calculateLocation(distances1, positions);
+        for(int i = 0; i < calcedLocation.length; i++) {
+            System.out.println(calcedLocation[i][0]);
+        }
     }
 }
